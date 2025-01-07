2dbc3cdb4f4eb5b2b734fdd98904a2faeb86d221
user-grinch <user.grinch@gmail.com>
2025-01-07 10:15:53 +0600

    Implement QR Scanner

--- lib/ui/qr_scanner_view.dart
+++ lib/ui/qr_scanner_view.dart
@@ -1,169 +1,126 @@
-import 'dart:developer';
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:flutter_contacts/properties/phone.dart' as fc_phone;
+import 'package:flutter_contacts/properties/email.dart' as fc_mail;
 
-class QRScannerView extends StatefulWidget {
-  const QRScannerView({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => _QRScannerViewState();
-}
-
-class _QRScannerViewState extends State<QRScannerView> {
-  Barcode? result;
-  QRViewController? controller;
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-
-  // In order to get hot reload to work we need to pause the camera if the platform
-  // is android, or resume the camera if the platform is iOS.
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    }
-    controller!.resumeCamera();
-  }
+class QRScannerView extends StatelessWidget {
+  const QRScannerView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(flex: 4, child: _buildQrView(context)),
-          Expanded(
-            flex: 1,
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  if (result != null)
-                    Text(
-                        'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
-                  else
-                    const Text('Scan a code'),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              await controller?.toggleFlash();
-                              setState(() {});
-                            },
-                            child: FutureBuilder(
-                              future: controller?.getFlashStatus(),
-                              builder: (context, snapshot) {
-                                return Text('Flash: ${snapshot.data}');
-                              },
-                            )),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              await controller?.flipCamera();
-                              setState(() {});
-                            },
-                            child: FutureBuilder(
-                              future: controller?.getCameraInfo(),
-                              builder: (context, snapshot) {
-                                if (snapshot.data != null) {
-                                  return Text(
-                                      'Camera facing ${describeEnum(snapshot.data!)}');
-                                } else {
-                                  return const Text('loading');
-                                }
-                              },
-                            )),
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            await controller?.pauseCamera();
-                          },
-                          child: const Text('pause',
-                              style: TextStyle(fontSize: 20)),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            await controller?.resumeCamera();
-                          },
-                          child: const Text('resume',
-                              style: TextStyle(fontSize: 20)),
-                        ),
-                      )
-                    ],
-                  ),
-                ],
+      appBar: AppBar(
+        title: const Text('Scan QR Code to add contact'),
+      ),
+      body: Stack(
+        children: [
+          MobileScanner(
+            scanWindow: Rect.fromCenter(
+              center: Offset(MediaQuery.of(context).size.width / 2,
+                  MediaQuery.of(context).size.height / 2),
+              width: 300,
+              height: 400,
+            ),
+            onDetect: (capture) async {
+              final List<Barcode> barcodes = capture.barcodes;
+              final Barcode? barcode = barcodes.first;
+              if (barcode != null && barcode.rawValue != null) {
+                String data = barcode.rawValue!;
+                if (_isValidVCard(data)) {
+                  await _addContact(data);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                        content: Text('Contact added successfully!')),
+                  );
+                  Navigator.of(context).pop();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Invalid vCard format!')),
+                  );
+                }
+              }
+            },
+          ),
+          Center(
+            child: Container(
+              width: 300,
+              height: 400,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 3),
+                borderRadius: BorderRadius.circular(16),
               ),
             ),
-          )
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Position the QR code within the frame to scan.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  shadows: [
+                    Shadow(
+                      color: Colors.black.withAlpha(128),
+                      offset: Offset(0, 1),
+                      blurRadius: 2,
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildQrView(BuildContext context) {
-    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
-    var scanArea = (MediaQuery.of(context).size.width < 400 ||
-            MediaQuery.of(context).size.height < 400)
-        ? 150.0
-        : 300.0;
-    // To ensure the Scanner view is properly sizes after rotation
-    // we need to listen for Flutter SizeChanged notification and update controller
-    return QRView(
-      key: qrKey,
-      onQRViewCreated: _onQRViewCreated,
-      overlay: QrScannerOverlayShape(
-          borderColor: Colors.red,
-          borderRadius: 10,
-          borderLength: 30,
-          borderWidth: 10,
-          cutOutSize: scanArea),
-      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
-    );
+  bool _isValidVCard(String data) {
+    return data.startsWith("BEGIN:VCARD") && data.endsWith("END:VCARD");
   }
 
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      this.controller = controller;
-    });
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-    });
-  }
+  Future<void> _addContact(String vCardData) async {
+    if (await FlutterContacts.requestPermission()) {
+      final Map<String, String> contactDetails = _parseVCard(vCardData);
 
-  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
-    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
-    if (!p) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('no Permission')),
+      final Contact newContact = Contact(
+        name: Name(first: contactDetails['FN'] ?? '', last: ''),
+        phones: contactDetails['TEL'] != null
+            ? [fc_phone.Phone(contactDetails['TEL']!)]
+            : [],
+        emails: contactDetails['EMAIL'] != null
+            ? [fc_mail.Email(contactDetails['EMAIL']!)]
+            : [],
       );
+
+      try {
+        await FlutterContacts.insertContact(newContact);
+        print('Contact added successfully!');
+      } catch (e) {
+        print('Error adding contact: $e');
+      }
+    } else {
+      print('Permission to access contacts denied!');
     }
   }
 
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
+  Map<String, String> _parseVCard(String vCardData) {
+    final Map<String, String> contactDetails = {};
+
+    for (String line in vCardData.split('\n')) {
+      if (line.startsWith('FN:')) {
+        contactDetails['FN'] = line.replaceFirst('FN:', '').trim();
+      } else if (line.startsWith('TEL:')) {
+        contactDetails['TEL'] = line.replaceFirst('TEL:', '').trim();
+      } else if (line.startsWith('EMAIL:')) {
+        contactDetails['EMAIL'] = line.replaceFirst('EMAIL:', '').trim();
+      }
+    }
+
+    return contactDetails;
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import dynamic_color
+import mobile_scanner
 import path_provider_foundation
 import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
