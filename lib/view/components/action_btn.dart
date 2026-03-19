a7706605c79a33aeb75a3db96652daabe36f168e
Grinch_ <user.grinch@gmail.com>
2026-03-19 18:37:44 +0600

    update: update call screen buttons to behave similar to md3ex buttons #28

--- lib/view/components/action_btn.dart
+++ lib/view/components/action_btn.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CallActionButton extends StatelessWidget {
+class CallActionButton extends StatefulWidget {
   final String label;
   final IconData icon;
   final Color backgroundColor;
@@ -8,6 +8,8 @@ class CallActionButton extends StatelessWidget {
   final VoidCallback onTap;
   final bool isLarge;
   final bool isSuperLarge;
+  final bool toggleable;
+  final bool initialOn;
 
   const CallActionButton({
     super.key,
@@ -18,47 +20,94 @@ class CallActionButton extends StatelessWidget {
     required this.onTap,
     this.isLarge = false,
     this.isSuperLarge = false,
+    this.toggleable = true,
+    this.initialOn = true,
   });
 
   @override
-  Widget build(BuildContext context) {
-    // Both Large and SuperLarge should probably show the text label
-    final showText = isLarge || isSuperLarge;
+  State<CallActionButton> createState() => _CallActionButtonState();
+}
 
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.fastOutSlowIn,
-      child: Material(
-        color: backgroundColor,
-        shape: showText
-            ? const StadiumBorder()
-            : RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-        clipBehavior: Clip.antiAlias,
+class _CallActionButtonState extends State<CallActionButton> {
+  bool isOn = true;
+  bool isPressed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isOn = widget.initialOn;
+  }
+
+  void _handleTap() {
+    if (widget.toggleable) {
+      setState(() {
+        isOn = !isOn;
+      });
+    }
+    widget.onTap();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final showText = widget.isLarge || widget.isSuperLarge;
+
+    BorderRadius currentRadius;
+    if (widget.toggleable) {
+      currentRadius =
+          isOn ? BorderRadius.circular(36) : BorderRadius.circular(24);
+    } else {
+      currentRadius =
+          isPressed ? BorderRadius.circular(24) : BorderRadius.circular(36);
+    }
+
+    return GestureDetector(
+      onTapDown: (_) {
+        if (!widget.toggleable) {
+          setState(() => isPressed = true);
+        }
+      },
+      onTapUp: (_) {
+        if (!widget.toggleable) {
+          setState(() => isPressed = false);
+        }
+      },
+      onTapCancel: () {
+        if (!widget.toggleable) {
+          setState(() => isPressed = false);
+        }
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.fastOutSlowIn,
+        height: 72,
+        width: widget.isSuperLarge ? double.infinity : null,
+        padding: EdgeInsets.symmetric(horizontal: showText ? 32 : 24),
+        decoration: BoxDecoration(
+          color: widget.backgroundColor,
+          borderRadius: currentRadius,
+        ),
         child: InkWell(
-          onTap: onTap,
-          child: Container(
-            height: 72,
-            width: isSuperLarge ? double.infinity : null,
-            padding: EdgeInsets.symmetric(horizontal: showText ? 32 : 24),
-            child: Row(
-              mainAxisSize: isSuperLarge ? MainAxisSize.max : MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(icon, color: foregroundColor, size: 28),
-                if (showText) ...[
-                  const SizedBox(width: 12),
-                  Text(
-                    label,
-                    style: TextStyle(
-                      color: foregroundColor,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 0.2,
-                    ),
+          borderRadius: currentRadius,
+          onTap: _handleTap,
+          child: Row(
+            mainAxisSize:
+                widget.isSuperLarge ? MainAxisSize.max : MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(widget.icon, color: widget.foregroundColor, size: 28),
+              if (showText) ...[
+                const SizedBox(width: 12),
+                Text(
+                  widget.label,
+                  style: TextStyle(
+                    color: widget.foregroundColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 0.2,
                   ),
-                ],
+                ),
               ],
-            ),
+            ],
           ),
         ),
       ),
